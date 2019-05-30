0934ff54a1e208a9f96c8b21e2ab509fcd6d2b7d
zongzi531 <zongzi.xy@gmail.com>
2019-05-30 11:50:44 +0800

    Update test

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -1,11 +1,43 @@
 import React from 'react'
 import EchartsMapChina from '../index'
-import renderer from 'react-test-renderer'
+import ReactTestRenderer from 'react-test-renderer'
 
-test('EchartsMapChina rendered', () => {
-  const component = renderer.create(
-    <EchartsMapChina />,
+test('Test DefaultProps.', () => {
+  let refs = null
+
+  ReactTestRenderer.create(
+    <EchartsMapChina
+      ref={ref => refs = ref}
+    />,
   )
-  // eslint-disable-next-line no-console
-  console.log(component)
+
+  const { props } = refs
+
+  expect(props.option).toEqual({})
+  expect(props.loadData()).toEqual([])
+  expect(props.getWarnMessage('jest test')).toEqual(console.warn('⚠️警告信息:', 'jest test'))
+  expect(props.backBtnKey).toEqual('my__back__btn__')
+  expect(props.getData()).toEqual(undefined)
+})
+
+test('Test omitProps.', () => {
+  const renderer = ReactTestRenderer.create(
+    <EchartsMapChina
+      option={{ test: 'test'}}
+      loadData={() => [1, 2]}
+      getWarnMessage={message => message}
+      backBtnKey="my__back__btn__test"
+      backBtnText="go Back"
+      backBtnIcon="null"
+      value={[1, 2]}
+      equalValue={() => true}
+      getData={() => [1, 2]}
+    />,
+  )
+
+  const { props } = renderer.toJSON()
+
+  for (const key of Object.keys(EchartsMapChina.propTypes)) {
+    expect(props).not.toHaveProperty(key)
+  }
 })
